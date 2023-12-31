d7df4eac02398d5db5b8d231b2bc867aa174bdcd
Tushar Inani <tusharinani93@gmail.com>
2023-12-31 12:10:23 +0000

    updated: main file to read json input and generate tasks

--- src/main.rs
+++ src/main.rs
@@ -2,44 +2,18 @@
 
 mod utils;
 mod types;
+mod tasks;
 
-// use utils::json_reader::read_json;
-// use types::scheduler_input::SchedulerInput;
+use utils::json_reader::read_json;
 use types::scheduler_input::SchedulerInput;
-// json_reader.rs
 
-use serde_json;
-
-use std::fs::File;
-use std::io::Read;
-
-
-pub fn read_json(file_path: &str) -> SchedulerInput {
-    // Open the file
-    let file = File::open(file_path).expect("Failed to open file");
-
-    // Read the file into a string
-    let mut buf_reader = std::io::BufReader::new(file);
-    let mut contents = String::new();
-    buf_reader.read_to_string(&mut contents).expect("Failed to read file");
-
-    // Parse the JSON string into a Schedule struct
-    serde_json::from_str(&contents).expect("Failed to parse JSON")
-}
+use crate::tasks::generator::generate_tasks;
 
 fn main() {
     // Your JSON file path
-    let file_path = "/tests/input.json";
-    let mut file = File::open(file_path).expect("Could not find the file on given path");
-
-    // read the file
-    let mut content = String::new();
-    // priocess of reading the file
-    file.read_to_string(&mut content).expect("Opps! Can't read the file");
-
-    // Call the read_json function from the json_reader module
+    let file_path = "./src/tests/input.json";
     let schedule: SchedulerInput = read_json(file_path);
-
+    generate_tasks(schedule);
+    // println!("{:#?}", schedule);
     // Now you can use the schedule struct as needed
-    println!("{:#?}", schedule);
 }
