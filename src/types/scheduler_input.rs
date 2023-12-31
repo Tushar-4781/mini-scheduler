fbded718c3e3e4fb49f162b05e54235f62fe6b68
Tushar Inani <tusharinani93@gmail.com>
2023-12-31 12:08:18 +0000

    updated: scheduler_input types

--- src/types/scheduler_input.rs
+++ src/types/scheduler_input.rs
@@ -1,30 +1,24 @@
 use serde::Deserialize;
 use std::collections::HashMap;
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct SIFilters {
     pub after_time: u32,
     pub before_time: u32,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct SIGoal {
     pub id: String,
     pub title: String,
     pub min_duration: u32,
-    pub start: String,
-    pub deadline: String,
-    pub filters: SIFilters,
+    pub start: Option <String>,
+    pub deadline: Option <String>,
+    pub filters: Option <SIFilters>,
 }
-
-#[derive(Debug, Deserialize)]
-pub struct SIGoals {
-    pub goals: HashMap<String, SIGoal>,
-}
-
 #[derive(Debug, Deserialize)]
 pub struct SchedulerInput {
-    pub start_ate: String,
+    pub start_date: String,
     pub end_date: String,
-    pub goals: SIGoals,
+    pub goals: HashMap<String, SIGoal>,
 }
