e6a131841a1db24387b91820dc685d2edb17aa74
Tushar Inani <tusharinani93@gmail.com>
2024-01-07 13:14:21 +0000

    defined: scheduler_engine types

--- src/types/mod.rs
+++ src/types/mod.rs
@@ -1,2 +1,3 @@
 pub mod scheduler_input;
 pub mod scheduler_output;
+pub mod scheduler_engine;
---
+++ src/types/scheduler_engine.rs
@@ -0,0 +1,7 @@
+use super::scheduler_output::Slot;
+
+
+struct FlexibleWeeklyGoal {
+    slot: Slot,
+    valid_days: Vec<String>,
+}
