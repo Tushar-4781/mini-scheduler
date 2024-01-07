89819ee23675697347465db1f62b17888fe66799
Tushar Inani <tusharinani93@gmail.com>
2024-01-07 13:14:35 +0000

    converted: types of slot fields

--- src/types/scheduler_output.rs
+++ src/types/scheduler_output.rs
@@ -5,7 +5,7 @@ pub struct Slot {
     pub goalid: String, // Replace TypeOfGoalId with the actual type of goal.id
     pub taskid: Uuid,
     pub title: String, // Replace TypeOfGoalTitle with the actual type of goal.title
-    pub start: i32, // Replace TypeOfStartTime with the actual type of after_time
-    pub deadline: i32, // Replace TypeOfDeadline with the actual type of before_time
-    pub duration: i32, // Replace TypeOfDuration with the actual type of total_duration
+    pub start: u32, // Replace TypeOfStartTime with the actual type of after_time
+    pub deadline: u32, // Replace TypeOfDeadline with the actual type of before_time
+    pub duration: u32, // Replace TypeOfDuration with the actual type of total_duration
 }
