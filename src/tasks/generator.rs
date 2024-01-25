31b6d479cc1af6c1ccdfa9ac7c5630ec0ab6ae21
Tushar Inani <tusharinani93@gmail.com>
2024-01-25 13:47:56 +0000

    updated generator

--- src/tasks/generator.rs
+++ src/tasks/generator.rs
@@ -1,15 +1,30 @@
 use crate::{
     tasks::splitter::split_goal,
-    types::{scheduler_input::SchedulerInput, scheduler_output::Slot},
+    types::{
+        scheduler_engine::{TBufferMap, TDueHrsMap},
+        scheduler_input::SchedulerInput,
+        scheduler_output::Slot,
+    },
 };
 
 use super::convertor::convert_into_task;
 
-pub fn generate_tasks(scheduler_input: SchedulerInput, calendar: &mut Vec<Vec<Slot>>) {
+pub fn generate_tasks(
+    scheduler_input: SchedulerInput,
+    mut calendar: &mut Vec<Vec<Slot>>,
+    mut buffer: &mut TBufferMap,
+    mut due_task_hrs: &mut TDueHrsMap,
+) {
     for (id, goal) in scheduler_input.goals {
         let splitted_goals = split_goal(goal);
         for splitted_goal in splitted_goals {
-            convert_into_task(&splitted_goal, calendar);
+            convert_into_task(
+                &splitted_goal,
+                &mut calendar,
+                &mut buffer,
+                &mut due_task_hrs,
+                &scheduler_input.start_date,
+            );
         }
     }
 }
