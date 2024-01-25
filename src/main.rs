0b4f92f917ed8e3b28a555954b8d983cd00c9173
Tushar Inani <tusharinani93@gmail.com>
2024-01-25 13:48:42 +0000

    created: calendar, duehrs, buffer variables

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,20 @@ mod tasks;
 mod types;
 mod utils;
 
+use std::collections::HashMap;
+
 use types::{scheduler_input::SchedulerInput, scheduler_output::Slot};
 use utils::json_reader::read_json;
 
-use crate::tasks::generator::generate_tasks;
+use crate::{
+    tasks::generator::generate_tasks,
+    types::scheduler_engine::{TBufferMap, TDueHrsMap},
+};
 
+fn print(mut x: i32) {
+    x = x + 2;
+    println!("{}", x);
+}
 fn main() {
     let file_path = "./src/tests/input.json";
     let schedule: SchedulerInput = read_json(file_path);
@@ -23,8 +32,12 @@ fn main() {
         vec![],
         vec![],
     ];
-
-    generate_tasks(schedule, &mut calendar);
+    let mut buffer: TBufferMap = HashMap::new();
+    let mut due_task_hrs: TDueHrsMap = HashMap::new();
+    generate_tasks(schedule, &mut calendar, &mut buffer, &mut due_task_hrs);
+    let mut x = 2;
+    print(x);
+    println!("{}", x);
     // println!("{:#?}", schedule);
     // Now you can use the schedule struct as needed
 }
